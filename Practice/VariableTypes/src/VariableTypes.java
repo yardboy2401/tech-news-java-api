95534d14fbdaedcdc9229c7beb9f56fb332f269b
Jeff Wymore <yardboy2401@gmail.com>
2022-08-01 11:29:14 -0700

    restructured file tree and folders

---
+++ Practice/VariableTypes/src/VariableTypes.java
@@ -0,0 +1,9 @@
+public class VariableTypes {
+    public static void main(String[] args) {
+        String name = "Cleopatra";
+        int age = 2016 + 69;
+        double price = 1999.99;
+        boolean truth = true;
+        boolean falseness = !truth;
+    }
+}
